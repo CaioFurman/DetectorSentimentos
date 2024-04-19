35ad9e1f38f84576856389bb5871f54b0ad16e9d
Caio Furman <caioxfurman@gmail.com>
2024-04-19 18:12:48 -0300

    Detecção por input

--- naive_bayes.py
+++ naive_bayes.py
@@ -19,11 +19,11 @@ X_train_counts = vectorizer.fit_transform(X_train)
 X_test_counts = vectorizer.transform(X_test)
 
 # Treinar o classificador Naive Bayes
-clf = MultinomialNB()
-clf.fit(X_train_counts, y_train)
+modelo = MultinomialNB()
+modelo.fit(X_train_counts, y_train)
 
 # Fazer previsões no conjunto de teste
-predictions = clf.predict(X_test_counts)
+predictions = modelo.predict(X_test_counts)
 
 # Avaliar a precisão do modelo
 accuracy = accuracy_score(y_test, predictions)
@@ -34,7 +34,26 @@ print("\nRelatório de Classificação:")
 print(classification_report(y_test, predictions))
 
 # Avaliar o modelo com validação cruzada
-cv_scores = cross_val_score(clf, X_train_counts, y_train, cv=5)
+cv_scores = cross_val_score(modelo, X_train_counts, y_train, cv=5)
 print("\nValidação Cruzada - Pontuações:")
 print(cv_scores)
 print("Precisão Média:", cv_scores.mean())
+
+def detector():
+    # Texto de entrada
+    input_text = input("\nInsira um texto para classificação: ")
+
+    # Pré-processamento do texto de entrada
+    input_text_counts = vectorizer.transform([input_text])
+
+    # Fazer previsões com o modelo
+    predicted_sentiment = modelo.predict(input_text_counts)
+
+    # Interpretar os resultados
+    print("Sentimento Previsto:", predicted_sentiment)
+
+continuar = True
+while(continuar):
+    detector()
+    escolha = input("\nDigite 1 para sair: o")
+    if escolha == '1': continuar = False
